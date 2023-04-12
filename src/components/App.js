e72c4028d2e70014e1af1dc1f83a16755b081d73
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-04-12 23:18:35 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,58 @@
+import React, { useState } from "react";
 
-import React from "react";
-import './../styles/App.css';
+function GenderForm() {
+  const [gender, setGender] = useState("");
+  const [shirtSize, setShirtSize] = useState("");
+  const [dressSize, setDressSize] = useState("");
 
-const App = () => {
+  const handleGenderChange = (e) => {
+    setGender(e.target.value);
+  };
+
+  const handleShirtSizeChange = (e) => {
+    setShirtSize(e.target.value);
+  };
+
+  const handleDressSizeChange = (e) => {
+    setDressSize(e.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Gender Form</h2>
+      <label>
+        Gender:
+        <select value={gender} onChange={handleGenderChange}>
+          <option value="">Please select</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+      </label>
+      <br />
+      {gender === "Male" && (
+        <label>
+          Shirt Size:
+          <select value={shirtSize} onChange={handleShirtSizeChange}>
+            <option value="">Please select</option>
+            <option value="Small">Small</option>
+            <option value="Medium">Medium</option>
+            <option value="Large">Large</option>
+          </select>
+        </label>
+      )}
+      {gender === "Female" && (
+        <label>
+          Dress Size:
+          <select value={dressSize} onChange={handleDressSizeChange}>
+            <option value="">Please select</option>
+            <option value="Small">Small</option>
+            <option value="Medium">Medium</option>
+            <option value="Large">Large</option>
+          </select>
+        </label>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default GenderForm;
